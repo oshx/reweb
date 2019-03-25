8ab9bba228939f47d276c5ade5aa3ae0486188fa
오승환 <oshxios@gmail.com>
2019-03-26 01:15:49 +0900

    Added dependencies for session

--- server/app.ts
+++ server/app.ts
@@ -2,24 +2,50 @@ import "@babel/polyfill";
 import * as Koa from "koa";
 import * as Router from "koa-router";
 import * as dotenv from "dotenv";
+import * as passport from "koa-passport";
+import * as session from "koa-session";
+import * as redisStore from "koa-redis";
+import * as bodyParser from "koa-bodyparser";
+import { SessionStore } from "koa-generic-session";
 
 dotenv.config();
 
 const app: Koa = new Koa();
 const router: Router = new Router();
 
-
 console.info(".env [TEST_VAR]=", process.env.TEST_VAR);
 console.info(".env [SERVER_PORT]=", process.env.SERVER_PORT);
 
 const globalRequestHandler: Koa.Middleware = async (ctx: Koa.Context,
   next: () => Promise<any>): Promise<void> => {
-    console.info("requested, ", ctx.request.URL.pathname, "[route for SPA]");
+  console.info("requested, ", ctx.request.URL.pathname, "[route for SPA]");
   ctx.body = `Node.js server is running on ${process.env.SERVER_PORT}`;
 };
 
-router.get('/*', globalRequestHandler);
+const storeOption: redisStore.RedisOptions = {
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+};
+const store: SessionStore = redisStore(storeOption);
 
+passport.serializeUser((user, done) => {
+  console.info("serializeUser,", user);
+  return done(null, user);
+});
+
+passport.deserializeUser((id, done) => {
+  console.info("deserializeUser,", id);
+  return done(null, id);
+});
+
+router.use();
+router.use(passport.initialize());
+router.use(passport.session());
+router.get("/*", session({
+  store,
+}, app), globalRequestHandler);
+
+app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
